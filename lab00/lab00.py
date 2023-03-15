7e284d637399b547623e4e646db8719d9c68d8f1
Lil-Rinnki <chenzq917@sjtu.edu.cn>
2023-03-16 00:40:41 +0800

    lab00

---
+++ lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    x = 2020 + 1 * 4 -1
+    return x
